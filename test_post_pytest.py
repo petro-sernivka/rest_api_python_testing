e0ec3a3b8210e83adb4476fa711e08aa1203d577
petro.sernivka <petro.sernivka@globallogic.com>
2021-03-04 15:30:52 +0200

    add test_post_status_code, test_post_hash_value functions

--- test_post_pytest.py
+++ test_post_pytest.py
@@ -1,26 +1,23 @@
 import requests
 import json
+import re
+
+url = 'https://httpbin.org/post'
+headers = {'Content-Type': 'application/json'}
+payload = {'key1': 1, 'key2': 'value2'}
+
+resp = requests.post(url, headers=headers, data=json.dumps(payload, indent=4))
 
 
-def test_post_headers_body_json():
-    url = 'https://httpbin.org/post'
-
-    # Additional headers.
-    headers = {'Content-Type': 'application/json'}
-
-    # Body
-    payload = {'key1': 1, 'key2': 'value2'}
-
-    # convert dict to json string by json.dumps() for body data.
-    resp = requests.post(url, headers=headers, data=json.dumps(payload, indent=4))
-
-    # Validate response headers and body contents, e.g. status code.
+def test_post_status_code():
     assert resp.status_code == 200
+
     resp_body = resp.json()
     assert resp_body['url'] == url
 
-    # print response full body as text
-    print(resp.text)
+    # assert resp_body['body']['status_code'] == 0
+    # assert re.match(r'[a-f0-9]{3}:[a-f0-9]{32}', resp_body['body']['hash_value'])
 
 
-test_post_headers_body_json()
+def test_post_hash_value():
+    assert re.match(r'[a-f0-9]{3}:[a-f0-9]{32}', 'a1b:905ad9252b784f6e8d1a172fb04e2271')

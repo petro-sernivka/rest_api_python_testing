8a7391955d721a5a251fea2dee34e295e10dbf92
petro.sernivka <petro.sernivka@globallogic.com>
2021-03-10 15:05:49 +0200

    used constants from consts.py

--- test_post_pytest.py
+++ test_post_pytest.py
@@ -3,12 +3,10 @@ import json
 import re
 import psycopg2
 
+from consts import *
 
-url = 'http://localhost:3000/'
-headers = {'Content-Type': 'application/json'}
-payload = {'email': 'asd@asd.com'}
 
-resp = requests.post(url=url, data=payload)
+resp = requests.post(url=URL, data=PAYLOAD)
 resp_body = resp.json()
 
 
@@ -19,16 +17,14 @@ def test_post_status_code():
 
 # Schema validation: fields name "status_code" (int type) and "hash_value" (str type)
 def test_schema_validation():
-    assert list(resp_body.keys()) == ['status_code', 'hash_value']
+    assert list(resp_body.keys()) == RESPONSE_SCHEMA_FIELDS
     assert isinstance(resp_body['status_code'], int)
     assert isinstance(resp_body['hash_value'], str)
 
 
 # "hash_value" matches a regular expression
 def test_post_hash_value():
-    hash_value_pattern = r'[a-f0-9]{3}:[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
-
-    assert re.match(pattern=hash_value_pattern, string=resp_body['hash_value'])
+    assert re.match(pattern=HASH_VALUE_PATTERN, string=resp_body['hash_value'])
 
 
 # Verifying the data was saved correctly (by SQL query)
@@ -36,9 +32,9 @@ def test_post_hash_value():
 
 # Verifying the same "hash_value" for multiple POST of the same email
 def test_multiple_post():
-    resp_1 = requests.post(url=url, data=payload)
+    resp_1 = requests.post(url=URL, data=PAYLOAD)
     resp_body_1 = resp_1.json()
-    resp_2 = requests.post(url=url, data=payload)
+    resp_2 = requests.post(url=URL, data=PAYLOAD)
     resp_body_2 = resp_2.json()
 
     assert resp_body_1['hash_value'] == resp_body_2['hash_value']
@@ -46,18 +42,10 @@ def test_multiple_post():
 
 # Verifying headers are as expected
 def test_headers_list():
-    expected_headers = ('x-powered-by',
-                        'content-type',
-                        'content-length',
-                        'etag',
-                        'date',
-                        'connection',
-                        'keep-alive')
-
     for header in resp.headers._store:
-        assert header in expected_headers
+        assert header in EXPECTED_HEADERS
 
 
 # Response time < 50 ms
 def test_response_time():
-    assert resp.elapsed.microseconds / 1000 < 50
+    assert resp.elapsed.microseconds / 1000 < ACCEPTABLE_RESPONSE_TIME

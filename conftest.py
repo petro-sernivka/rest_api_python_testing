30bf522652920df2f16851c024d1f17010a107b4
petro.sernivka <petro.sernivka@globallogic.com>
2021-03-11 18:17:15 +0200

    added setup_database fixture to conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,21 @@
 import pytest
+import psycopg2
 import requests
-from utils.models import user as user_model
-
-# from config import endpoints
 
 
-# @pytest.fixture
-# def user():
-#     user_body = user_model()
-#     response = requests.post(url=endpoints.USERS, data=user_body)
-#     return user_body, response.json()['id']
+@pytest.fixture
+def setup_database():
+    def connect():
+        conn = psycopg2.connect(
+            host="localhost",
+            database="uuid",
+            user="postgres",
+            password="postgres")
+
+        cur = conn.cursor()
+        return cur
+
+    yield connect
+
+    # cur.close()
+    # conn.close()

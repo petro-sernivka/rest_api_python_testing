5c4ce0f28c4d2890b9e7c5e6bf02ba341775e2d5
petro.sernivka <petro.sernivka@globallogic.com>
2021-03-15 10:57:45 +0200

    added assert messages to test_post_pytest.py

--- tests/test_post_pytest.py
+++ tests/test_post_pytest.py
@@ -1,7 +1,5 @@
 import requests
-import json
 import re
-import psycopg2
 
 from rest_api_python_testing.consts import *
 
@@ -9,40 +7,36 @@ resp = requests.post(url=URL, data=PAYLOAD)
 resp_body = resp.json()
 
 
+# Positive tests
 # POST request returns 200 status code
 def test_post_status_code():
-    assert resp.status_code == 200
+    assert resp.status_code == 200, \
+        f'Response status code should be 200, not {resp.status_code}'
+    assert resp.reason == 'OK', \
+        f'"{resp.reason}" message returned instead of "OK"'
 
 
 # Schema validation: fields name "status_code" (int type) and "hash_value" (str type)
 def test_schema_validation():
-    assert list(resp_body.keys()) == RESPONSE_SCHEMA_FIELDS
-    assert isinstance(resp_body['status_code'], int)
-    assert isinstance(resp_body['hash_value'], str)
+    assert list(resp_body.keys()) == RESPONSE_SCHEMA_FIELDS, \
+        f'Expected fields: {RESPONSE_SCHEMA_FIELDS}, actual fields: {list(resp_body.keys())}'
+    assert isinstance(resp_body['status_code'], int), \
+        f'"status_code" field should be "int" type, not "{type(resp_body["status_code"]).__name__}"'
+    assert isinstance(resp_body['hash_value'], str), \
+        f'"status_code" field should be "str" type, not "{type(resp_body["hash_value"]).__name__}"'
 
 
 # "hash_value" matches a regular expression
 def test_post_hash_value():
-    assert re.match(pattern=HASH_VALUE_PATTERN, string=resp_body['hash_value'])
+    assert re.match(pattern=HASH_VALUE_PATTERN, string=resp_body['hash_value']), \
+        f'"hash_value" does not match a pattern: xxx:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx, ' \
+        f'actual "hash_value": {resp_body["hash_value"]}'
 
 
-# Verifying the data was saved correctly (by SQL query)
+# Verifying the data was saved correctly
 def test_data_is_posted():
-    # cur = setup_database
-    conn = psycopg2.connect(
-        host="localhost",
-        database="uuid",
-        user="postgres",
-        password="postgres")
-
-    cur = conn.cursor()
-    cur.execute(f"SELECT * FROM users WHERE email='{PAYLOAD['email']}'")
-    query_result = cur.fetchall()
-
-    cur.close()
-    conn.close()
-
-    assert query_result[0][0] == resp_body['hash_value']
+    assert resp_body['status_code'] == 0, \
+        f'"status_code" should be 0, not {resp_body["status_code"]}'
 
 
 # Verifying the same "hash_value" for multiple POST of the same email
@@ -52,15 +46,32 @@ def test_multiple_post():
     resp_2 = requests.post(url=URL, data=PAYLOAD)
     resp_body_2 = resp_2.json()
 
-    assert resp_body_1['hash_value'] == resp_body_2['hash_value']
+    assert resp_body_1['hash_value'] == resp_body_2['hash_value'], \
+        f'Multiple POST of the same email should return the same "hash_value"\n' \
+        f'First "hash_value" for {PAYLOAD["email"]} is {resp_body_1["hash_value"]}\n' \
+        f'Second "hash_value" for {PAYLOAD["email"]} is {resp_body_2["hash_value"]}\n'
 
 
 # Verifying headers are as expected
 def test_headers_list():
     for header in resp.headers._store:
-        assert header in EXPECTED_HEADERS
+        assert header in EXPECTED_HEADERS, \
+            f'"{header}" header is not in expected headers list {EXPECTED_HEADERS}'
 
 
 # Response time < 50 ms
 def test_response_time():
-    assert resp.elapsed.microseconds / 1000 < ACCEPTABLE_RESPONSE_TIME
+    assert resp.elapsed.microseconds / 1000 < ACCEPTABLE_RESPONSE_TIME, \
+        f'Expected response time: {ACCEPTABLE_RESPONSE_TIME} ms, ' \
+        f'actual response time: {resp.elapsed.microseconds / 1000} ms'
+
+
+# Negative tests
+# Invalid value for email parameter
+# Invalid values in HTTP headers
+# Unsupported methods for endpoints
+
+
+# Destructive tests
+# Empty value for email parameter
+# Incorrect HTTP headers

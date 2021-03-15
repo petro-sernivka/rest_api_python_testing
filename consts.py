019ff626c0018276fc8c2530301cec1bf8e76847
petro.sernivka <petro.sernivka@globallogic.com>
2021-03-15 16:56:06 +0200

    added EMPTY_EMAIL_MESSAGE to consts.py

--- consts.py
+++ consts.py
@@ -29,6 +29,7 @@ INVALID_EMAILS = ('address',
                   'Abc..123@example.com')
 
 INVALID_EMAIL_MESSAGE = 'Email is not valid'
+EMPTY_EMAIL_MESSAGE = 'Email is needed to make such request'
 
 INVALID_HEADER_VALUE = {'Content-Type': 'not_valid_value'}
 

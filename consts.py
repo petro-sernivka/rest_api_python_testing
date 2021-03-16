1b7a95b4e29fec7456a83a38c24d39919cd20653
petro.sernivka <petro.sernivka@globallogic.com>
2021-03-16 16:10:35 +0200

    added "server" to consts.py

--- consts.py
+++ consts.py
@@ -1,5 +1,7 @@
+import socket
+
 # POST request params
-URL = 'http://localhost:3000/'
+URL = f'http://{socket.gethostbyname(socket.gethostname())}:3000/'
 PAYLOAD = {'email': 'test@test.com'}
 
 RESPONSE_SCHEMA_FIELDS = ['status_code', 'hash_value']
@@ -7,7 +9,7 @@ RESPONSE_SCHEMA_FIELDS = ['status_code', 'hash_value']
 HASH_VALUE_PATTERN = r'[a-f0-9]{3}:[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
 
 EXPECTED_HEADERS = ('content-type', 'content-length', 'etag', 'date', 'connection', 'keep-alive', 'x-powered-by',
-                    'access-control-allow-origin')
+                    'access-control-allow-origin', 'server')
 
 ACCEPTABLE_RESPONSE_TIME = 50
 

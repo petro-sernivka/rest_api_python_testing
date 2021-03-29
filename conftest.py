99f0d295db2dd0fdfb3b0431ce12ff49d5f66ccb
petro.sernivka <petro.sernivka@globallogic.com>
2021-03-29 14:28:24 +0300

    unused setup_database fixture

--- conftest.py
+++ conftest.py
@@ -4,18 +4,18 @@ import requests
 
 
 # @pytest.fixture
-def setup_database():
-    def connect():
-        conn = psycopg2.connect(
-            host="localhost",
-            database="uuid",
-            user="postgres",
-            password="postgres")
-
-        cur = conn.cursor()
-        return cur
-
-    yield connect
+# def setup_database():
+#     def connect():
+#         conn = psycopg2.connect(
+#             host="localhost",
+#             database="uuid",
+#             user="postgres",
+#             password="postgres")
+#
+#         cur = conn.cursor()
+#         return cur
+#
+#     yield connect
 
     # cur.close()
     # conn.close()
